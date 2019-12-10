14deaf77c75d6be67e9be8a46a6f03b304da538c
ArthurAbeshouse <artieabe99@gmail.com>
2019-12-10 15:34:33 -0500

    updated file 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for n in range(ord('a'), ord('z') + 1):
-    if i != 101 and i != 113:
-	print("{}".format(chr(n)), end="")
+    if n != ord('q') and n != ord('e'):
+        print("{}".format(chr(n)), end="")
