9d16dd74c64cfd7b6d71d04618e4c90c832af24b
ArthurAbeshouse <artieabe99@gmail.com>
2019-12-19 18:42:59 +0000

    fixed pep8 errors

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3                                                              
+#!/usr/bin/python3
 def squared(x):
     return x**2
 
+
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
+    new_m = []
     for l in matrix:
-        new_matrix += [list(map(squared, l))]
-    return new_matrix
+        new_m += [list(map(squared, l))]
+    return new_m
