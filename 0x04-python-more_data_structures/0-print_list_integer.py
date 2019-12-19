3767a89cdc81687afd3b8078ade57c1fb4ae99e7
ArthurAbeshouse <artieabe99@gmail.com>
2019-12-19 18:39:45 +0000

    removed unnecssary file

--- 0x04-python-more_data_structures/0-print_list_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def squared(x):
-    return x**2
-
-def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for l in matrix:
-        new_matrix += [list(map(squared, l))]
-    return new_matrix
