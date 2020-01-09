37fc590b180ed0fc28ab6fd021b25c6a44bc3002
ArthurAbeshouse <artieabe99@gmail.com>
2020-01-09 23:21:03 +0000

    changed size to value

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,15 +10,14 @@ class Square:
 
     @property
     def size(self):
-        """size properties"""
         return self.__size
 
     @size.setter
     def size(self, value):
         """size setter"""
-        if type(size) is not int:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
