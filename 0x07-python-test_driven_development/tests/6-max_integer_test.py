ffaa1287de619e898d1baf59be0efcb65770e47f
ArthurAbeshouse <artieabe99@gmail.com>
2020-01-13 20:10:34 +0000

    finalized test cases and created 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,3 +1,36 @@
 #!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
 
+class TestMaxInteger(unittest.TestCase):
 
+    def test_docstring(self):
+        doc = __import__('6-max_integer').__doc__
+        self.assertIsNotNone(doc)
+        self.assertIsNotNone(max_integer.__doc__)
+
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_positive(self):
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
+
+    def test_negative(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4, -5]), -1)
+
+    def test_one_item(self):
+        self.assertEqual(max_integer([5]), 5)
+
+    def test_float(self):
+        self.assertEqual(max_integer([1.7, 2.4, 3.9, 4.6]), 4.6)
+
+    def test_string(self):
+        self.assertRaises(TypeError, max_integer, [1, 2, "World"])
+
+    def test_character(self):
+        self.assertEqual(max_integer(['a', 'b', 'c']), 'c')
+
+    def test_word(self):
+        self.assertEqual(max_integer(["Hello", "World"]), "World")
