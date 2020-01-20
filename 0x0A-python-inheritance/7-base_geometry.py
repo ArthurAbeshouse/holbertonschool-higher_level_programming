5d8ff4721ebb8e861350d00c02c3ccdba4350107
ArthurAbeshouse <artieabe99@gmail.com>
2020-01-20 19:59:33 +0000

    validates the value

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """place holder for area calculation"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """method for validating the value"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
