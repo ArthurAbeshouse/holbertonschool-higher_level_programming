413893e6542edfe6d2d65d48ece8b6010499de47
ArthurAbeshouse <artieabe99@gmail.com>
2020-01-21 21:26:28 +0000

    fixed spacing

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,14 +3,13 @@
 
 class Student:
     """ class for jsonification """
+
     def __init__(self, first_name, last_name, age):
-    """ constructor """
+        """ constructor """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
-    def to_json(self):
-    """ retrieves dictionary """
-        return dict(vars(self))
 
-    
+    def to_json(self):
+        """ retrieves dictionary """
+        return dict(vars(self))
