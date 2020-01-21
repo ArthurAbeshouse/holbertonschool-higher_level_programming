a7bf2cb9b049384408d5be8d24792fcee3856aa7
ArthurAbeshouse <artieabe99@gmail.com>
2020-01-21 21:55:35 +0000

    returns a list of lists of integers representing the Pascal’s triangle of integers

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    """ solves pascal's triangle """
+    rows = [[1 for j in range(i + 1)] for i in range(n)]
+    for n in range(n):
+        for i in range(n - 1):
+            rows[n][i + 1] = sum(rows[n - 1][i:i + 2])
+    return rows
