fcf8cf2bdf0e3cfcc9ddc2c8dba99d411d35074e
ArthurAbeshouse <artieabe99@gmail.com>
2020-03-29 02:15:30 +0000

    lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+"""Lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                               user=argv[1], passwd=argv[2], database=argv[3])
+    cur = database.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    database.close()
