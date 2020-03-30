8b06adfef4b1716c818fe191f40ab37ac2e8a5c9
ArthurAbeshouse <artieabe99@gmail.com>
2020-03-30 15:41:24 +0000

    pep8 fixes

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -10,7 +10,8 @@ from model_city import City
 if __name__ == "__main__":
 
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3]),
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(argv[1], argv[2], argv[3]),
         pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
