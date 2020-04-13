05447947a175f29d58d84a621ccea92ec96f30b2
ArthurAbeshouse <artieabe99@gmail.com>
2020-04-13 22:28:46 +0000

    added try and except

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,9 +4,12 @@ from requests import get
 from sys import argv
 
 if __name__ == "__main__":
-        url = get("https://api.github.com/repos/{}/{}/commits".format(
-                argv[2], argv[1])).json()
-        for i in range(0, 10):
-            print("{}: {}".format(url[i].get("sha"),
-                                  url[i].get("commit").get(
-                                          "author").get("name")))
+        try:
+                url = get("https://api.github.com/repos/{}/{}/commits".format(
+                        argv[2], argv[1])).json()
+                for i in range(0, 10):
+                        print("{}: {}".format(url[i].get("sha"),
+                                              url[i].get("commit").get(
+                                                      "author").get("name")))
+        except:
+                pass
