5b9282e9369c0dffb351e1d3a24cdc0b6091d770
ArthurAbeshouse <artieabe99@gmail.com>
2020-04-13 18:10:24 +0000

    fixed name

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t- type: {}".format(type(read)))
         print("\t- content: {}".format(read))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+        print("\t- utf8 content: {}".format(read.decode("utf-8")))
