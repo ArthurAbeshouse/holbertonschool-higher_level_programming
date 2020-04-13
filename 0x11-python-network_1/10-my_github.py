c7c2d476c8948f76d593a3bf6061e4fc92ca03be
ArthurAbeshouse <artieabe99@gmail.com>
2020-04-13 21:57:33 +0000

    My Github!

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Uses the Github API to display your id"""
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    r = get(url, auth=(argv[1], argv[2])).json()
+
+    try:
+        print(r["id"])
+    except KeyError:
+        print("No results")
