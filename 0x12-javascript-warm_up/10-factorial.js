4ec0696fa4859d160faedc48d9c44fd816c730f1
ArthurAbeshouse <artieabe99@gmail.com>
2020-05-05 17:48:53 +0000

    fixed spacing

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 function factorial (i) {
   if (i === 0 || isNaN(i) || i < 2) {
-    return 1;  
+    return 1;
+  } else {
+    return i * factorial(i - 1);
   }
-  return (i * factorial(i - 1));
 }
 console.log(factorial(process.argv[2]));
