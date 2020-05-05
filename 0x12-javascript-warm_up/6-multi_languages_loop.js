9a2b396a814550849894e653ed52bb8bd989040e
ArthurAbeshouse <artieabe99@gmail.com>
2020-05-05 17:39:48 +0000

    fixed spacing

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let p of list) {
-  console.log(p);
+for (let p = 0; p < list.length; p++) {
+  console.log(list[p]);
 }
